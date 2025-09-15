e9f8df9f61a89cd1cc9bbab4d4218083382184ac
briannalytical <briannamcneil.dev@gmail.com>
2025-09-15 17:17:35 -0400

    dailybudget methods

--- src/main/java/com/briannalytical/Voyagr/Models/DailyBudget.java
+++ src/main/java/com/briannalytical/Voyagr/Models/DailyBudget.java
@@ -1,6 +1,8 @@
 package com.briannalytical.Voyagr.Models;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.util.List;
 
 public class DailyBudget {
     private Trip trip;
@@ -32,4 +34,39 @@ public class DailyBudget {
     public void setBudgetAmount(BigDecimal budgetAmount) {
         this.budgetAmount = budgetAmount;
     }
+
+
+    public BigDecimal getAssumedDailyBudget(List<CategoryBudget> categoryBudgets, int totalTripDays) {
+        BigDecimal totalCategoryBudgets = BigDecimal.ZERO;
+        for (CategoryBudget budget : categoryBudgets) {
+            totalCategoryBudgets = totalCategoryBudgets.add(budget.getBudgetAmount());
+        }
+        return totalCategoryBudgets.divide(new BigDecimal(totalTripDays), RoundingMode.HALF_UP);
+    }
+
+    public BigDecimal getAllocatedDailyBudget(List<CategoryBudget> categoryBudgets, int totalTripDays) {
+        BigDecimal totalAllocated = BigDecimal.ZERO;
+        for (CategoryBudget categoryBudget : categoryBudgets) {
+            BigDecimal dailyPortion = categoryBudget.getBudgetAmount().divide(new BigDecimal(totalTripDays));
+            totalAllocated = totalAllocated.add(dailyPortion);
+        }
+        return totalAllocated;
+    }
+
+    public BigDecimal getActualDailySpent(List<Expense> tripExpenses) {
+        BigDecimal totalActualSpent = BigDecimal.ZERO;
+        for (Expense expense : tripExpenses) {
+            if (expense.getDate().toLocalDate().equals(this.date)) {
+                totalActualSpent = totalActualSpent.add(expense.getAmount());
+            }
+        }
+        return totalActualSpent;
+    }
+
+    public BigDecimal getDailyBudgetOverUnder(List<CategoryBudget> categoryBudgets, List<Expense> tripExpenses, int totalTripDays) {
+        BigDecimal allocated = getAllocatedDailyBudget(categoryBudgets, totalTripDays);
+        BigDecimal actual = getActualDailySpent(tripExpenses);
+        return allocated.subtract(actual);
+    }
+
 }
