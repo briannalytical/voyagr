9d08ab1eee049e3e079626466bb9bb29f2200fdc
Brianna McNeile <briannamcneil.dev@gmail.comm>
2025-10-05 20:15:40 -0400

    tests

---
+++ src/test/java/com/briannalytical/Voyagr/CategoryBudgetTest.java
@@ -0,0 +1,12 @@
+package com.briannalytical.Voyagr;
+
+import org.junit.jupiter.api.Test;
+
+public class CategoryBudgetTest {
+
+    @Test
+    public void calculateBudgetSpent_ForAccommodation_ReturnsActualBudgetSpent() {
+        // Arrange
+
+    }
+}
